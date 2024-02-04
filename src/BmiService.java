054802433f4ce097680493e5a5290db9dc745f95
oxanaP <oxichka@list.ru>
2024-02-04 18:09:38 +0700

    Changed Main and BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,15 +1,8 @@
 public class BmiService {
-    public int calculate(int ves, double rost, int e_izm) {
-        double result;
-        System.out.println("Вес: " + ves + " кг.");
-        if (e_izm == 1) {//сантиметры
-            double rost_sm = rost / 100;
-            result = ves / Math.pow(rost_sm, 2);
-            System.out.println("Рост: " + (int) rost + " см.");
-        } else { //  метры
-            result = ves / Math.pow(rost, 2);
-            System.out.println("Рост: " + rost + " м.");
-        }
+    public int calculate(int weight, double height) {
+        System.out.println("Вес: " + weight+ " кг.");
+        System.out.println("Рост: " + height + " м.");
+        double result = weight / Math.pow(height, 2);
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService bmService = new BmiService();
-        int imt;
-        int p_ves = 98;
-        double p_rost = 1.87;
-        int p_e_izm = 0; // 0 - метр, 1 - сантиметр
-        imt = bmService.calculate(p_ves, p_rost, p_e_izm);
-
-        System.out.println("Индекс массы тела: " + imt);
+        BmiService bmiService = new BmiService();
+        int weightKilogram = 98;
+        double heightMetre = 1.87;
+        int bodyMassIndex = bmiService.calculate(weightKilogram, heightMetre);
+        System.out.println("Индекс массы тела: " + bodyMassIndex);
     }
 }
